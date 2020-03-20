e8ac9abe62acaac86d1a6f19f0d6e04aeb2dcbdc
Codetheist <56620151+Codetheist@users.noreply.github.com>
2020-03-20 16:33:07 -0400

    Updating blog dir with form configuration

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView  # new
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -10,7 +11,7 @@ class BlogListView(ListView):
     template_name = 'home.html'
 
 
-class BlogDetailView(DetailView):  # new
+class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
@@ -19,3 +20,15 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
